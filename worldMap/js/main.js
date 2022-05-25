9a281ec1634b6f2222e475289de66e5b761dcfaa
vfedirko2022 <72541949+vfedirko2022@users.noreply.github.com>
2022-05-25 23:38:07 +0300

    Add files via upload

---
+++ worldMap/js/main.js
@@ -0,0 +1,11 @@
+function scoreAdd() {
+    score__number.innerHTML = parseInt(score__number.innerText) + 1;
+}
+
+function scoreRemove() {
+    score__number.innerHTML = parseInt(score__number.innerText) - 1;
+}
+
+function scoreReset() {
+    score__number.innerHTML = 0;
+}
